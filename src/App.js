1d082ddc8f20151a00ce977ec2a0e429c643dede
IdrisGit <idris.gadi01@gmail.com>
2022-05-15 02:25:29 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,170 @@
+import React, {useState} from "react";
+
+import Wrapper from "./Components/Wrapper";
+import Screen from "./Components/Screen";
+import ButtonBox from "./Components/ButtonBox";
+import Button from "./Components/Button";
+
+const btnValues = [
+  ["C", "+-", "%", "/"],
+  [7, 8, 9, "X"],
+  [4, 5, 6, "+"],
+  [1, 2, 3, "-"],
+  [0, ".", "="],
+];
+
+const thousandMarkSpaces = (num) => {
+ return String(num).replace(/(?<!\..*)(\d)(?=(?:\d{3})+(?:\.|$))/g, "$1 ");
+};
+
+const removeSpaces = (num) => num.toString().replace(/\s/g, "");
+
+
+const App =() =>{
+    
+  let [calc, setCalc] = useState({
+    sign : "",
+    num : 0,
+    result : 0,
+  });
+
+  //numClickHandler
+  const numClickHandler = (e) => {
+    e.preventDefault();
+    const value = e.target.innerHTML;
+    if (removeSpaces(calc.num).length < 16){
+      setCalc({
+       ...calc,
+       num : 
+          calc.num === 0 && value === "0"
+            ? "0"
+            : removeSpaces(calc.num)  % 1 === 0 
+            ? thousandMarkSpaces(Number(removeSpaces(calc.num + value))) 
+            : thousandMarkSpaces(calc.num + value),
+        result : !calc.sign ? 0 : calc.result,
+      });
+    }
+  };
+
+  //commaClickHandler
+  const commaClickHandler = (e) => {
+    e.preventDefault();
+    const value = e.target.innerHTML;
+
+    setCalc({
+      ...calc,
+      num : !calc.num.toString().includes(".") ? calc.num + value : calc.num,
+    });
+  };
+
+  //signClickHandler
+  const signClickHandler = (e) => {
+    e.preventDefault();
+    const value = e.target.innerHTML;
+    setCalc({
+      ...calc,
+      sign : value,
+      result : !calc.result && calc.num ? calc.num : calc.result,
+      num : 0,
+    });
+  };
+
+  //equalsClickHandler
+  const equalsClickHandler = () => {
+    if (calc.sign && calc.num) {
+      const math = (a, b, sign) =>
+        sign === "+"
+          ? a + b
+          : sign === "-"
+          ? a - b
+          : sign === "X"
+          ? a * b
+          : a / b;
+
+      setCalc({
+        ...calc,
+        result : calc.num === "0" && calc.sign === "/"
+                ? "Math Error"
+                : thousandMarkSpaces(
+                    math (
+                      Number(removeSpaces(calc.result)), 
+                      Number(removeSpaces(calc.num)), 
+                      calc.sign
+                    )
+                  ),
+        sign : "",
+        num : 0,
+      });
+    }
+  };
+
+  //invertClickHandler
+  const invertClickHandler = () => {
+    setCalc ({
+      ...calc,
+      num : calc.num ? thousandMarkSpaces(removeSpaces(calc.num) * -1 ) : 0,
+      result : calc.result ? thousandMarkSpaces(removeSpaces(calc.result * -1 )) : 0,
+      sign : "",
+    });
+  };
+
+  //percerntClickHandler
+  const percentClickHandler = () => {
+    let num = calc.num ? parseFloat(calc.num) : 0;
+    let result = calc.result ? parseFloat(calc.result) : 0;
+
+    setCalc({
+      ...calc,
+      num : (num /= Math.pow(100,1)),
+      result : (result /= Math.pow(100,1)),
+      sign : "",
+    });
+  };
+
+  //resetClickHandler
+  const resetClickHandler = () => {
+    setCalc({
+      ...calc,
+      sign : "",
+      num : 0,
+      result : 0,
+    });
+  };
+
+  return(
+    <Wrapper>
+      <Screen value = {calc.num ? calc.num : calc.result}/>
+      <ButtonBox>
+      {
+        btnValues.flat().map((btn, i) => {
+            return(
+              <Button
+                key ={i}
+                className={btn === "=" ? "equals" : ""}
+                value={btn}
+                onclick= {
+                  btn === "C"
+                    ? resetClickHandler
+                    : btn === "+-"
+                    ? invertClickHandler
+                    : btn === "%"
+                    ? percentClickHandler
+                    : btn === "="
+                    ? equalsClickHandler
+                    : btn === "/" || btn === "X" || btn === "-" || btn === "+"
+                    ? signClickHandler
+                    : btn === "."
+                    ? commaClickHandler
+                    : numClickHandler
+                }
+              />
+            );
+          })
+        }
+      </ButtonBox>
+    </Wrapper>
+  );
+};
+
+
+export default App;
